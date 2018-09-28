58af91d5d2af95f33f4704aef45f432b8f0a29e3
Ricky Mason <rickster797@hotmail.co.uk>
2018-09-28 13:20:16 +0100

    have added logic to push winners into winner array in the state

--- src/Components/TournamentBracket.jsx
+++ src/Components/TournamentBracket.jsx
@@ -6,65 +6,64 @@ class TournamentBracket extends Component {
   constructor(props) {
     super(props);
 
-    const players = this.props.players;
+    this.state = this.init(this.props.players, 1);
+  }
+
+  init(players, roundNumber) {
     const matches = [];
-
     shuffle(players);
-
     while (players.length >= 2) {
       const match = [players.pop(), players.pop()];
-
-      /** Set initial style(s) and resultTxt(s). */
       match[0].style = buttonNeutralStyle;
       match[1].style = buttonNeutralStyle;
       match[0].resultTxt = '';
       match[1].resultTxt = '';
-
       matches.push(match);
     }
-
-    this.state = {
+    return {
       players: players,
       matches: matches,
       winners: [],
+      roundNumber: roundNumber
     };
   }
 
   /** matchWinner needs to be array index of 0 or 1 (for left or right player). */
   setResult(matchIdx, matchWinner) {
-
-    /** Gets the current match. */
     const matches = this.state.matches;
     const match = matches[matchIdx];
-    const winners = this.state.winners;
-
-    /** Sets the winners, styles and resultTxt. */
+    const winners = [];
     match[0].win = (matchWinner === 0);
     match[1].win = (matchWinner === 1);
     match[0].style = this.computeButtonStyle(matchIdx, 0)
     match[1].style = this.computeButtonStyle(matchIdx, 1)
     match[0].resultTxt = (matchWinner === 0) ? ' wins' : ' loses'
     match[1].resultTxt = (matchWinner === 1) ? ' wins' : ' loses'
-
-    /** Replaces the match (in the state) with the updated version
-    & add's winner to winners array ;) kinda */
     matches[matchIdx] = match;
     this.setState({ matches: matches });
+    try {
+      for (let i = 0; i < matches.length; i++) {
+        if (matches[i][0].win) {
+          winners.push(matches[i][0]);
+        } else if (matches[i][1].win) {
+          winners.push(matches[i][1]);
+        }
+      }
+      this.setState({ winners: winners });
+    } catch (e) {
+    }
 
-    if(matchWinner === 1) {
-        winners.push(match); 
-    } else if (matchWinner === 0) {
-        winners.splice(match); 
-    } else {
-       
+    if (winners.length >= this.state.matches.length) {
+
+      /** TODO - Continue here... */
+      console.warn('Winners Array is complete!', winners);
+      console.log(this.state);
+
     }
   }
 
   computeButtonStyle(matchIdx, playerIdx) {
-
-    /** Gets the current match. */
-    let match = this.state.matches[matchIdx];
-
+    const match = this.state.matches[matchIdx];
     if(!match[0].win && !match[1].win) {
       return buttonNeutralStyle;          // If no winners..
     } else if(match[playerIdx].win) {
@@ -75,10 +74,10 @@ class TournamentBracket extends Component {
   }
 
   render() {
-    console.log(this.state)
+    const roundOver = this.state.roundOver;
     return (
       <Fragment>
-        <h2>Round 1... Fight!</h2>
+        <h2>Round { this.state.roundNumber }... Fight!</h2>
         {this.state.matches.map((match, i) => (
         <div key={i} style={ divStyle }>
           <p style={matchesBox}>{ match[0].value }&nbsp;<span style={ spanStyle }>VS</span>&nbsp;{ match[1].value }</p>
